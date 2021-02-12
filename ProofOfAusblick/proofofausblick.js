044c3a00ee23eb5cf46a1f2da4092dcf65854aab
veronika-hai <veronika.haidarow@stud.h-da.de>
2021-02-12 19:04:42 +0100

    ProofOfAusblick?

---
+++ ProofOfAusblick/proofofausblick.js
@@ -0,0 +1,68 @@
+let video; // webcam
+
+// Bilderkennung => Gestenerkennung
+let mobilenet;
+let classifier;
+let gesturelabel;
+
+// Bild als Geste -> kann Classifier in Bildern Gesten erkennen?
+let bild1;
+
+// API importieren
+function preload() {
+  mobilenet = ml5.featureExtractor("MobileNet", { numLabels: 3 }, modelReady);
+}
+
+function setup() {
+  // Videokamera capturen
+  createCanvas(640, 480);
+  video = createCapture(VIDEO);
+  video.size(640, 480);
+  video.hide();
+
+  classifier = mobilenet.classification(bild1, customModelReady);
+}
+
+// Laden das von uns gespeicherte Modell hoch
+function modelReady() {
+  console.log("Model is ready!!!");
+  classifier.load("model.json", customModelReady);
+}
+
+function customModelReady() {
+  console.log("CM ready!!");
+  classifier.classify(gotGestures);
+}
+
+function videoReady() {
+  console.log("Video is ready!!!");
+}
+
+// BILDERKENNUNG
+// gibt uns die Namen der Gesten zurück
+function gotGestures(error, result) {
+  if (error) {
+    console.error(error);
+  } else {
+    gesturelabel = result[0].label; // result[0] ist die Geste mit der höchsten Wahrscheinlichkeit
+    console.log(result);
+    classifier.classify(gotGestures);
+  }
+}
+
+function imageReady() {
+  image(bild1, 0, 0, width, height);
+}
+
+function draw() {
+  clear();
+  image(video, 0, 0);
+  bild1 = createImg("hilfe.jpg", imageReady);
+  bild1.hide();
+
+  // unten links steht, welche Geste die Maschine erkennt
+  // die Geste, mit der höchsten Wahrscheinlichkeit wird angezeigt
+  fill(255);
+  textSize(16);
+  text(gesturelabel, 10, height - 10);
+}
