533d8366993f739f0dad4d51b606e6384dcc1a59
veronika-hai <veronika.haidarow@stud.h-da.de>
2021-02-28 22:20:44 +0100

    startVid() wird angezeigt, aber löst nicht aus

--- Interaktive Doku/script.js
+++ Interaktive Doku/script.js
@@ -3,6 +3,10 @@ let img2 = document.createElement("img");
 img2.src = "Sound.png";
 
 let btnstart = document.getElementById("startbtn");
+let btnsound = document.createElement("button");
+btnsound.id = "soundan";
+btnsound.innerHTML = "Sound an!";
+btnsound.setAttribute("onclick", "startVid()");
 
 let vid1 = document.getElementById("catcall1");
 
@@ -22,10 +26,32 @@ function changeImg() {
       fill: "forwards",
     }
   );
+  if (btnstart.style.display === "none") {
+    btnstart.style.display = "block";
+  } else {
+    btnstart.style.display = "none";
+  }
 
   setTimeout(function replace1() {
     img1.replaceWith(img2);
+    btnstart.replaceWith(btnsound);
     img2.animate(
+      [
+        {
+          opacity: 0,
+        },
+        {
+          opacity: 1,
+        },
+      ],
+      {
+        duration: 1500,
+        iterations: 1,
+        fill: "forwards",
+      }
+    );
+
+    btnsound.animate(
       [
         {
           opacity: 0,
@@ -42,17 +68,17 @@ function changeImg() {
     );
   }, 1500);
 
-  if (btnstart.style.display === "none") {
-    btnstart.style.display = "block";
-  } else {
-    btnstart.style.display = "none";
-  }
+  console.log(btnstart.attributes);
+  console.log(btnsound.attributes);
 }
 window.changeImg = changeImg;
 
 function startVid() {
   vid1.style.display = "block";
+  if (btnsound.style.display === "none") {
+    btnsound.style.display = "block";
+  } else {
+    btnsound.style.display = "none";
+  }
 }
 window.startVid = startVid;
-
-console.log(btnsound.style.display);
